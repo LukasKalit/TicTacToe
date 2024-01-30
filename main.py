65aa2d22d7cee4fcff67f89ac215388832687695
LukasKalit <lukaskalit@gmail.com>
2024-01-30 01:45:58 +0100

    Before implementing simple ai

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ def CheckEnd():
         elif Tic[0][2] == Tic[1][1] == Tic[2][0] != " ":
             Winner = Tic[1][1]
             return Winner
-
-
     return None
+
+def make_ai_move():
+    return "1","1"
+
 
 Tic = [
     [" "," "," "],
@@ -28,18 +30,25 @@ Tic = [
     ]
 Winner = ""
 WinnerPlayer = ""
-letter = ["O","X"]
-i = 0
+letter = "O"
 
 while WinnerPlayer == "":
     print(f" {Tic[0][0]} | {Tic[0][1]} | {Tic[0][2]} \n-----------\n {Tic[1][0]} | {Tic[1][1]} | {Tic[1][2]} \n-----------\n {Tic[2][0]} | {Tic[2][1]} | {Tic[2][2]}")
     user_input_x, user_input_y = None, None
     correct_coordinates = True
     while correct_coordinates:
-        while user_input_x not in ["0", "1", "2"]:
-            user_input_x = input(f"Enter your row coordinates for {letter[i]}:  ")
-        while user_input_y not in ["0", "1", "2"]:
-            user_input_y = input(f"Enter your column coordinates for {letter[i]}:  ")
+        if letter == "O":
+            
+            while user_input_x not in ["0", "1", "2"]:
+                user_input_x = input(f"Enter your row coordinates for {letter}:  ")
+            while user_input_y not in ["0", "1", "2"]:
+                user_input_y = input(f"Enter your column coordinates for {letter}:  ")
+
+        else:
+            move_ai = make_ai_move()
+            user_input_x, user_input_y = move_ai
+        
+        
         if Tic[int(user_input_x)][int(user_input_y)] == " ":
             correct_coordinates = False
         else:
@@ -47,16 +56,16 @@ while WinnerPlayer == "":
             user_input_y = ""
             print("place already occupied")
 
-    if i == 0:
-        Tic[int(user_input_x)][int(user_input_y)] = "O"
-        i = 1
-    elif i == 1:
-        Tic[int(user_input_x)][int(user_input_y)] = "X"
-        i = 0
+    Tic[int(user_input_x)][int(user_input_y)] = letter
     result = CheckEnd()
     if result in ["O", "X", False]:
         WinnerPlayer = False
     user_input_x, user_input_y = None, None
+
+    if letter == "O":
+        letter = "X"
+    else:
+        letter = "O"
 
 if result == "O":
     print("Player 1 win")
